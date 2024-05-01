1617ab378a689c868b47b64dd373af09bc382030
igorferrati <ferrati.igor@gmail.com>
2024-05-01 17:45:19 -0300

    add post

--- crud/main.go
+++ crud/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/servidor"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 func main() {
 
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
 
 	fmt.Println("Servidor up em localhost:5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
