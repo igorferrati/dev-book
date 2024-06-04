017aab24f7f70efeb82c5cd6009ae3ccec2c213a
igorferrati <ferrati.igor@gmail.com>
2024-06-03 21:11:45 -0300

    correção nick

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -6,7 +6,7 @@ import "time"
 type Usuario struct {
 	ID       uint64    `json:"id, omitempty`
 	Nome     string    `json:"nome, omitempty`
-	Nice     string    `json:"nick, omitempty`
+	Nick     string    `json:"nick, omitempty`
 	Email    string    `json:"email, omitempty`
 	Senha    string    `json:"senha, omitempty`
 	CriadoEm time.Time `json:"CriadoEm, omitempty`
