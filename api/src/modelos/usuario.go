b0d0d131ff237b65d98732e6ca3c22d7db1fa3ac
igorferrati <ferrati.igor@gmail.com>
2024-06-06 21:06:13 -0300

    add tratativas

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string    `json:"email, omitempty`
 	Senha    string    `json:"senha, omitempty`
 	CriadoEm time.Time `json:"CriadoEm, omitempty`
+}
+
+// Preparar chama métodos de validar e formatar usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O campo nome é obrigatório.")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O campo nick é obrigatório.")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O campo e-mail é obrigatório.")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("O campo senha é obrigatório.")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
