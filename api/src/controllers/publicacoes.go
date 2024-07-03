653431790c444f4bd0064be68adda4ebd703ad7b
igorferrati <ferrati.igor@gmail.com>
2024-07-02 21:46:46 -0300

    add publicacoes

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,13 +1,56 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
-//CriarPublicacao cria uma nova publicacão
+// CriarPublicacao cria uma nova publicacão
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
-//BuscarPublicacoes traz as publicacoes do feed do usuário
+// BuscarPublicacoes traz as publicacoes do feed do usuário
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
 
 }
@@ -17,7 +60,7 @@ func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//AtualizarPublicacao altera os dados de uma publicacão
+// AtualizarPublicacao altera os dados de uma publicacão
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 }
