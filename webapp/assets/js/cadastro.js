64af42588f37a548c4ada7b34ba277649975b88d
igorferrati <ferrati.igor@gmail.com>
2024-07-17 20:44:42 -0300

    wip

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -3,4 +3,21 @@ $('#formulario-cadastro').on('submit', criarUsuario);
 function criarUsuario(evento) {
     evento.preventDefault();
     console.log("Teste");
+
+    if ( $('#senha').val() != $('#confirmar-senha').val() ) {
+        alert("Senhas devem ser iguais");
+        return;
+    }
+
+    $.ajax({
+        url: "/usuarios",
+        method: "POST",
+        data: { 
+            nome: $('#nome').val(),
+            email: $('#email').val(),
+            nick: $('#nick').val(),
+            senha: $('#senha').val()
+        }
+    })
+
 }
